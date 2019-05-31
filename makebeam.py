277aa98ed9b0d1c438e9e68b178608dcd457bbe4
Kyle Kabasares <46383246+kylekaba@users.noreply.github.com>
2019-05-31 13:23:21 -0700

    Add files via upload

---
+++ makebeam.py
@@ -0,0 +1,51 @@
+def make_beam(grid_size=99, res=1., amp=1., x0=0., y0=0., x_std=1., y_std=1., rot=0., fits_name=None):  # CONFIRMED
+    """
+    Generate a beam spread function
+    :param grid_size: size of grid (must be odd!)
+    :param res: resolution of the grid (arcsec/pixel)
+    :param amp: amplitude of the 2d gaussian
+    :param x0: mean of x axis of 2d gaussian
+    :param y0: mean of y axis of 2d gaussian
+    :param x_std: FWHM of beam in x (to use for standard deviation of Gaussian) (in arcsec)
+    :param y_std: FWHM of beam in y (to use for standard deviation of Gaussian) (in arcsec)
+    :param rot: rotation angle in radians
+    :param fits_name: this name will be the filename to which the beam fits file is written (if None, write no file)
+    return the synthesized beam
+    """
+
+    # SET RESOLUTION OF BEAM GRID THE SAME AS FLUXES GRID
+    x_beam = [0.] * grid_size
+    y_beam = [0.] * grid_size
+
+    # set center of the beam grid axes (find the central pixel number along each axis: same for x_beam and y_beam)
+    ctr = (len(x_beam) + 1.) / 2.  # +1 bc python starts counting at 0, grid_size is odd
+
+    # grid_size must be odd, so fill in the axes with resolution * ((i+1) - ctr)!
+    for i in range(len(x_beam)):
+        x_beam[i] = res * ((i + 1) - ctr)  # (arcsec/pix) * N_pixels = arcsec
+        y_beam[i] = res * ((i + 1) - ctr)  # (arcsec/pix) * N_pixels = arcsec
+
+    # SET UP MESHGRID
+    xx, yy = np.meshgrid(x_beam, y_beam)
+
+    # SET UP PSF 2D GAUSSIAN VARIABLES
+    n = 2.35482  # Convert from FWHM to std_dev: http://mathworld.wolfram.com/GaussianFunction.html
+    x_std /= n
+    y_std /= n
+    a = np.cos(rot) ** 2 / (2 * x_std ** 2) + np.sin(rot) ** 2 / (2 * y_std ** 2)
+    b = -np.sin(2 * rot) / (4 * x_std ** 2) + np.sin(2 * rot) / (4 * y_std ** 2)
+    c = np.sin(rot) ** 2 / (2 * x_std ** 2) + np.cos(rot) ** 2 / (2 * y_std ** 2)
+
+    # CALCULATE PSF, NORMALIZE IT TO AMPLITUDE
+    synth_beam = np.exp(-(a * (xx - x0) ** 2 + 2 * b * (xx - x0) * (yy - y0) + c * (yy - y0) ** 2))
+    A = amp / np.amax(synth_beam)  # normalize it so the input amplitude is the real amplitude
+    synth_beam *= A
+
+    # IF write_fits, WRITE PSF TO FITS FILE
+    # if fits_name is not None:
+    #if not Path(fits_name).exists():
+        #hdu = fits.PrimaryHDU(synth_beam)
+        #hdul = fits.HDUList([hdu])
+        #hdul.writeto(fits_name)
+
+    return synth_beam
